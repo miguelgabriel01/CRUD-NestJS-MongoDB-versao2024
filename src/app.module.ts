28c09137bc17ac2395fa04c1c5b40af2f6637a1f
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2024-06-07 23:45:18 -0300

    feature: criação do metodo de usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://miguelgabriel11:bolso22@nestjsusers.raralhq.mongodb.net/'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
